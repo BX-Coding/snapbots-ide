1c18afe0cac1ad38ac8a36f6b905c45f954c56af
DuncanAJohnson <duncanjohnson99@gmail.com>
2025-03-04 18:04:57 -0500

    more verbose test

--- api/test.js
+++ api/test.js
@@ -1,9 +1,14 @@
 export default function handler(req, res) {
-    res.status(200).json({ 
-      message: 'API route is working',
-      method: req.method,
-      query: req.query,
-      headers: req.headers,
-      url: req.url
-    });
-  }
+  // Set CORS headers to allow all origins
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Return a simple JSON response
+  res.status(200).json({ 
+    message: 'API route is working',
+    method: req.method,
+    query: req.query,
+    path: req.url
+  });
+}
